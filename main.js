939f9f170a8bfdfc34cec8825f4cdbe163c2a40d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-01-26 22:53:52 +0900

    city 取得

--- main.js
+++ main.js
@@ -2,19 +2,27 @@ const weatherForm = document.getElementById("weather-form");
 const cityInput = document.getElementById("city-input");
 const weatherResults = document.getElementById("weather-results");
 
-fetch(
-  ""
-)
-  .then((response) => response.json())
-  .then(
-    (jsonData) =>
-      (weatherResults.innerHTML = `
-      <div class="results-country">${jsonData.location.country}</div>
-      <div class="results-city">${jsonData.location.name}</div>
-      <div class="results-temp">${jsonData.current.temp_c}<span>C</span></div>
-      <div class="results-condition">
-        <img src="https:${jsonData.current.condition.icon}" alt="icon">
-        <span>${jsonData.current.condition.text}</span>
-      </div>
-    `)
-  );
+weatherForm.addEventListener("submit", e => {
+  e.preventDefault();
+  getWeather(cityInput.value);
+});
+
+const getWeather = (city) => {
+  fetch(
+    `&q=${city}&aqi=no`
+  )
+    .then((response) => response.json())
+    .then(
+      (jsonData) =>
+        (weatherResults.innerHTML = `
+        <div class="results-country">${jsonData.location.country}</div>
+        <div class="results-city">${jsonData.location.name}</div>
+        <div class="results-temp">${jsonData.current.temp_c}<span>C</span></div>
+        <div class="results-condition">
+          <img src="https:${jsonData.current.condition.icon}" alt="icon">
+          <span>${jsonData.current.condition.text}</span>
+        </div>
+      `)
+    );
+}
+

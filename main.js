8432d074e6ad08d168a3a58f0e86876ca3651d2a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-01-26 22:41:42 +0900

    天気予報表示

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-const weatherForm = document.getElementById('weather-form');
-const cityInput = document.getElementById('city-input');
-const weatherResults = document.getElementById('weather-results');
+const weatherForm = document.getElementById("weather-form");
+const cityInput = document.getElementById("city-input");
+const weatherResults = document.getElementById("weather-results");
+
+fetch(
+  ""
+)
+  .then((response) => response.json())
+  .then(
+    (jsonData) =>
+      (weatherResults.innerHTML = `
+      <div class="results-country">${jsonData.location.country}</div>
+      <div class="results-city">${jsonData.location.name}</div>
+      <div class="results-temp">${jsonData.current.temp_c}<span>C</span></div>
+      <div class="results-condition">
+        <img src="https:${jsonData.current.condition.icon}" alt="icon">
+        <span>${jsonData.current.condition.text}</span>
+      </div>
+    `)
+  );

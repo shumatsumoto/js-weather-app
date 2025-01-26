9b4b73512556408bb66b77ca83bafa4dd5b41d6e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-01-26 22:14:52 +0900

    JS要素取得

---
+++ main.js
@@ -0,0 +1,3 @@
+const weatherForm = document.getElementById('weather-form');
+const cityInput = document.getElementById('city-input');
+const weatherResults = document.getElementById('weather-results');
